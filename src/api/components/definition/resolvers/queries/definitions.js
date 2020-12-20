c8bd863db008cb18e77f27c3e3e3dead387d3f4e
3kezoh <3kezoh@gmail.com>
2020-12-20 06:51:50 +0100

    definitions query

--- src/api/components/definition/resolvers/queries/definitions.js
+++ src/api/components/definition/resolvers/queries/definitions.js
@@ -7,9 +7,10 @@ const DEFINITIONS_PER_PAGE = 5;
 const definitions = async (_, { filter, page = 1 }) => {
   if (filter) validate({ filter });
   const conditions = { ...(filter || {}) };
-  if (has(conditions, "word")) conditions.word = escapeRegExp(conditions.word);
+  const hasWord = has(conditions, "word");
+  if (hasWord) conditions.word = escapeRegExp(conditions.word);
   return Definition.find(conditions)
-    .sort("-score createdAt")
+    .sort(hasWord ? "-score createdAt" : "-createdAt")
     .skip((page - 1) * DEFINITIONS_PER_PAGE)
     .limit(DEFINITIONS_PER_PAGE)
     .populate("author");
